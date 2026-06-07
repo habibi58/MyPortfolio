754417108f39ef28ffd7549182bda484f5c8df2c
habibi58 <jasonceloza90@gmail.com>
2026-06-07 20:49:34 +0800

    phone responsiveness 3.2

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -220,10 +220,9 @@ interface MobileLabelProps {
   project: Project;
   index: number;
   isActive: boolean;
-  visual: ProjectVisual;
 }
 
-const MobileLabel = ({ project, index, isActive, visual }: MobileLabelProps) => (
+const MobileLabel = ({ project, index, isActive }: MobileLabelProps) => (
   <div
     className="pss-mobile-label"
     style={{
@@ -614,7 +613,6 @@ export const ProjectsSection = () => {
                 project={project}
                 index={index}
                 isActive={activeIndex === index}
-                visual={projectVisuals[index] || projectVisuals[0]}
               />
 
               <ScrollStackItem
